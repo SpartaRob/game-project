82fecb0c9c0fb0134acbc9d4e2dca67b87f66893
R Whitehouse <rwhitehouse@spartaglobal.com>
2018-02-06 15:36:18 +0000

    Added Reset Button, Header text style has been undated

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   // alert('Welcome to Connect 4.')
 
-  var board = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0], [0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]];
+  // var board = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0], [0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]];
 
   var boxes = $("button");
   var currentPlayer = 'Player 1';
@@ -10,29 +10,53 @@ $(document).ready(function() {
 
   console.log(boxes);
 
+  $('#reset').on('click', resetBoard)
+  $('.board button').on('click', addPiece)
 
-  $('.board button').on('click', function() {
+  function addPiece(event){
     if ($(this).hasClass('default')) {
       if (currentPlayer === 'Player 1') {
-        $('#player-turn').text("  " + currentPlayer);
+        currentPlayer = 'Player 2';
+        $('#player-turn').text("Current player is: " + currentPlayer);
         $(this).addClass('player1Selected');
         $(this).removeClass('default');
         counter++;
-        currentPlayer = 'Player 2';
       } else {
-        $('#player-turn').text("  " + currentPlayer);
+        currentPlayer = 'Player 1';
+        $('#player-turn').text("Current player is: " + currentPlayer);
         $(this).addClass('player2Selected');
         $(this).removeClass('default');
         counter++;
-        currentPlayer = 'Player 1';
       }
     } else {
       alert('cell already selected please chose another');
     }
-  });
+    if (counter >= 43) {
+      $('#player-turn').text("The game is a draw")
+      var run = prompt("This game is a draw, would you like to play again? \n 1. (y)es \n 2. (n)o")
+
+      if (run !== 'n' && run !== 'y'){
+        while (run !== 'n' && run !== 'y') {
+          alert("Invalid input, please input y for yes or n for no");
+
+          run = prompt("Would you like to play again? \n 1. (y)es \n 2. (n)o");
+        }
+      } else if (run === 'y') {
+        resetBoard();
+      } else {
+        alert("Thank you playing");
+      }
+    }
+  }
+
+  function resetBoard(){
+    $('#player-turn').text("Current player is: Player 1");
+    counter = 1;
+    $('.board button').addClass('default');
+    $('.board button').removeClass('player1Selected');
+    $('.board button').removeClass('player2Selected');
+  }
 
 
 
-
-
-  });
+});
