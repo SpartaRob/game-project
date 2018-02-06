109e50ef734f6b8d6ec1c76583b63a29795708ea
R Whitehouse <rwhitehouse@spartaglobal.com>
2018-02-06 18:38:33 +0000

    End of day 1

--- js/app.js
+++ js/app.js
@@ -1,18 +1,16 @@
 $(document).ready(function() {
 
-  // alert('Welcome to Connect 4.')
-
   // var board = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0], [0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]];
 
   var boxes = $("button");
   var currentPlayer = 'Player 1';
   var counter = 1;
 
-  console.log(boxes);
+  $('#reset').click(resetBoard)
+  $('.board button').click(addPiece)
 
-  $('#reset').on('click', resetBoard)
-  $('.board button').on('click', addPiece)
 
+  //Add a piece to the board, removes default class styling, adds new styling depending active player, checks for a draw
   function addPiece(event){
     if ($(this).hasClass('default')) {
       if (currentPlayer === 'Player 1') {
@@ -31,10 +29,22 @@ $(document).ready(function() {
     } else {
       alert('cell already selected please chose another');
     }
+    checkForWin();
+    checkForDraw();
+  }
+
+  function checkForWin(){
+
+  }
+
+  //Counter adds total pieces which have been placed and if the board is full without a winner will end the game
+  function checkForDraw(){
     if (counter >= 43) {
       $('#player-turn').text("The game is a draw")
       var run = prompt("This game is a draw, would you like to play again? \n 1. (y)es \n 2. (n)o")
 
+
+      //Input Validation for prompt box
       if (run !== 'n' && run !== 'y'){
         while (run !== 'n' && run !== 'y') {
           alert("Invalid input, please input y for yes or n for no");
@@ -49,6 +59,8 @@ $(document).ready(function() {
     }
   }
 
+
+  //Resets the board, clears all styling, resets the turn counter and status message
   function resetBoard(){
     $('#player-turn').text("Current player is: Player 1");
     counter = 1;
@@ -56,7 +68,5 @@ $(document).ready(function() {
     $('.board button').removeClass('player1Selected');
     $('.board button').removeClass('player2Selected');
   }
-
-
 
 });
